7ead929bf82f3ae1d52b162b7ccec96d541d441c
Julien Déoux <juliendeoux@gmail.com>
2023-06-30 22:41:44 +0200

    Rust LSP

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -77,6 +77,11 @@ return {
             on_attach = on_attach,
         })
 
+        lspconfig.rust_analyzer.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
+
         lspconfig.graphql.setup({
             capabilities = capabilities,
             on_attach = on_attach,
