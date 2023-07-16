37d70371031bb2d7f7af7dbaa1ca51acb794ce4b
Julien Déoux <juliendeoux@gmail.com>
2023-07-16 09:33:58 +0200

    Everforest theme for tmux

--- fish/functions/set_dark_mode.fish
+++ fish/functions/set_dark_mode.fish
@@ -1,3 +1,4 @@
 function set_dark_mode
     kitty @ set-colors --all ~/.config/kitty/colors/dark.conf
+    tmux source-file ~/.config/tmux/dark.conf
 end
--- fish/functions/set_light_mode.fish
+++ fish/functions/set_light_mode.fish
@@ -1,3 +1,4 @@
 function set_light_mode
     kitty @ set-colors --all ~/.config/kitty/colors/light.conf
+    tmux source-file ~/.config/tmux/light.conf
 end
