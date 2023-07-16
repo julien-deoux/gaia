a406fbd53e1d50762c3ff509826f721bddac00b5
Julien Déoux <juliendeoux@gmail.com>
2023-07-16 17:23:49 +0200

    pnpm in $PATH

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,10 @@ source /Users/julien/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 if [ $KITTY_WINDOW_ID ]
     apply_system_mode
 end
+
+# pnpm
+set -gx PNPM_HOME "/Users/julien/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
