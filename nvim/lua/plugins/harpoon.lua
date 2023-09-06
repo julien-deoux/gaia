bdf0eb6ced1b192033a86a46046df70ef663ec99
Julien Déoux <julien@hiresweet.com>
2023-09-06 15:11:35 +0200

    Wider harpoon for longer file paths

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,19 +1,24 @@
 return {
-    "ThePrimeagen/harpoon",
-    keys = {
-        "<leader>a",
-        "<leader>fm",
-    },
-    config = function()
-        local set = vim.keymap.set
-        set("n", "<leader>a", function()
-            require("harpoon.mark").add_file()
-        end)
-        set("n", "<leader>fm", function()
-            require("harpoon.ui").toggle_quick_menu()
-        end)
-    end,
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    }
+	"ThePrimeagen/harpoon",
+	keys = {
+		"<leader>a",
+		"<leader>fm",
+	},
+	config = function()
+		local set = vim.keymap.set
+		require("harpoon").setup({
+			menu = {
+				width = vim.api.nvim_win_get_width(0) - 20,
+			},
+		})
+		set("n", "<leader>a", function()
+			require("harpoon.mark").add_file()
+		end)
+		set("n", "<leader>fm", function()
+			require("harpoon.ui").toggle_quick_menu()
+		end)
+	end,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 }
