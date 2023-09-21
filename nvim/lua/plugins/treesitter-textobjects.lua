e5317e46382a1c13cfe1f656fc5a1719029e3d7f
Julien Déoux <juliendeoux@gmail.com>
2023-09-21 09:01:15 +0200

    Textobjects

---
+++ nvim/lua/plugins/treesitter-textobjects.lua
@@ -0,0 +1,21 @@
+return {
+	"nvim-treesitter/nvim-treesitter-textobjects",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			textobjects = {
+				select = {
+					enable = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@comment.outer",
+						["ic"] = "@comment.inner",
+					},
+				},
+			},
+		})
+	end,
+}
