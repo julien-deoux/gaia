0dbec16956c62d8e7fe5e492cf9c3e9ef8affcb9
Julien Déoux <juliendeoux@gmail.com>
2023-12-28 01:46:39 +0100

    Treesitter join rules for rescript

--- nvim/lua/plugins/treesj.lua
+++ nvim/lua/plugins/treesj.lua
@@ -1,14 +1,25 @@
 return {
-    "Wansmer/treesj",
-    keys = {
-        "<space>m",
-        "<space>j",
-        "<space>s",
-    },
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-        require("treesj").setup()
-    end,
+	"Wansmer/treesj",
+	keys = {
+		"<space>m",
+		"<space>j",
+		"<space>s",
+	},
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		local lang_utils = require("treesj.langs.utils")
+
+		require("treesj").setup({
+			langs = {
+				rescript = {
+					function_type_parameters = lang_utils.set_preset_for_args(),
+					arguments = lang_utils.set_preset_for_args(),
+					record_type = lang_utils.set_preset_for_dict(),
+					record = lang_utils.set_preset_for_dict(),
+				},
+			},
+		})
+	end,
 }
