61bb00b8f602f5d9cfbdfbc7f6a74a70727d9ab7
Julien Déoux <julien@hiresweet.com>
2024-01-02 11:21:18 +0100

    Hurl filetype detection

--- nvim/lua/core/filetypes.lua
+++ nvim/lua/core/filetypes.lua
@@ -4,3 +4,10 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 		vim.opt.filetype = "reason"
 	end,
 })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = { "*.hurl" },
+	callback = function()
+		vim.opt.filetype = "hurl"
+	end,
+})
