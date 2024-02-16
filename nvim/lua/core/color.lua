afe6bf1aed6af49a3a2bb24d9838a4a1b6fa0082
Julien Déoux <juliendeoux@gmail.com>
2024-02-16 17:48:06 +0100

    Easier theme switching in neovim

--- nvim/lua/core/color.lua
+++ nvim/lua/core/color.lua
@@ -1,31 +1,25 @@
 local setmode = function(mode)
-    vim.opt.background = mode
+	vim.opt.background = mode
 end
 
 local apply_system_mode = function()
-    local output = vim.fn.system({ "defaults", "read", "-g", "AppleInterfaceStyle" })
-    if output == "Dark\n" then
-        setmode("dark")
-    else
-        setmode("light")
-    end
+	local output = vim.fn.system({ "defaults", "read", "-g", "AppleInterfaceStyle" })
+	if output == "Dark\n" then
+		setmode("dark")
+	else
+		setmode("light")
+	end
 end
 
-vim.api.nvim_create_user_command(
-    'ColorsDark',
-    function() setmode("dark") end,
-    { desc = "Set dark mode" }
-)
-vim.api.nvim_create_user_command(
-    'ColorsLight',
-    function() setmode("light") end,
-    { desc = "Set light mode" }
-)
+vim.api.nvim_create_user_command("ColorsDark", function()
+	setmode("dark")
+end, { desc = "Set dark mode" })
+vim.api.nvim_create_user_command("ColorsLight", function()
+	setmode("light")
+end, { desc = "Set light mode" })
 
-vim.api.nvim_create_user_command(
-    'ColorsApplySystem',
-    apply_system_mode,
-    { desc = "Read system mode" }
-)
+vim.api.nvim_create_user_command("ColorsApplySystem", apply_system_mode, { desc = "Read system mode" })
+
+vim.keymap.set("n", "<leader>as", apply_system_mode)
 
 apply_system_mode()
