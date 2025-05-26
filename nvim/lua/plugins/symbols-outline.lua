aac04ec58b1dccce9d5123e31493c93db74487a3
Julien Déoux <juliendeoux@gmail.com>
2025-05-26 09:44:07 +0200

    Updated outline plugin

--- nvim/lua/plugins/symbols-outline.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"simrat39/symbols-outline.nvim",
-	config = function()
-		require("symbols-outline").setup({
-			autofold_depth = 0,
-		})
-		vim.keymap.set("n", "<leader>o", ":SymbolsOutline<cr>")
-	end,
-}
